a4af5dc0eef9a7c52bc318f48d524c0247e1ac74
nitin-das <nitin.das@thomson.co.uk>
2019-08-22 16:20:47 +0100

    Initla Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_word():
+    return 'Hey, we have Flask in a Docker container!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
